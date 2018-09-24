d36d3f44ce0f98e2b25108a7952b611f855a45c2
Sinan Erdinç <sinan.erdinc@trendyol.com>
2018-09-24 21:31:44 +0300

    döngü örneği ve özel response yakalama örneği eklendi.

--- lesson2.py
+++ lesson2.py
@@ -9,17 +9,20 @@ def print_log(param):
     print(param)
 
 def not_exist(l):
+    """Response yakalama ve success olarak işaretleme örneği."""
     with l.client.get("/does_not_exist/", catch_response=True) as response:
         print("Response status code:", response.status_code)
         print("Response content:", response.text)
         if response.status_code == 404:
             response.success()
 
-def random_page(l):
-    l.client.get(random.choice(website_url))
+def loop_page(l):
+    """Teste isim verme ve döngü parametre geçme örneği."""
+    for i in range(1,10):
+        l.client.get("/blog?id=%i" % i, name="/blog?id=[id]")
 
 class UserBehavior(TaskSet):
-    tasks = {not_exist:1}
+    tasks = {not_exist:1, loop_page:3}
 
     def on_start(self):
         print_log("Test Başlıyor.")
